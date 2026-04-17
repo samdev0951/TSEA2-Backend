361c0fec05e00afd40c1a528c43ddd3e74e3610a
Syed-MAF <syed.maf06@outlook.com>
2026-04-18 00:39:58 +0100

    Update courses.py
    changed bits

--- api/courses.py
+++ api/courses.py
@@ -5,6 +5,8 @@ from database.models.User import User
 from sqlalchemy.orm import Session
 from stores.CourseStore import CourseStore
 
+from fastapi import APIRouter, Depends, HTTPException
+
 router = APIRouter(tags=["courses"], prefix="/courses")
 
 @router.get("/")
@@ -18,14 +20,14 @@ def course(course_slug: str):
 
 #Endpoint: GET /api/courses/{course_id}/{content_id}
 
-@router.get("/courses/{course_id}/{content_id}")
+@router.get("/{course_id}/{content_id}")
 def get_content(course_id: str, content_id: str):
-    content = CourseStore.get_content(course_id, content_id)
+    content = CourseStore.get__course_content(course_id, content_id)
     if content is None:
         raise HTTPException(status_code=404, detail="Content not found")
     return content
 
-
+# 
 
 
 
